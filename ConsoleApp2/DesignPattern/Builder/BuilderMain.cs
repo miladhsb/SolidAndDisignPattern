8d089f1a2e04e1274875d85443c0b9a5ea2554b0
milad khoshkam <milad@gmail.com>
2022-07-01 13:55:55 +0430

    Builder

---
+++ ConsoleApp2/DesignPattern/Builder/BuilderMain.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.DesignPattern.Builder
+{
+
+
+    public abstract class IBuilder
+    {
+        protected Car car { get; set; }=new Car();
+        public abstract void CreateName();
+        public abstract void CreateModel();
+        public abstract void CreateColor();
+
+        public Car GetCar()
+        {
+            return car;
+        }
+
+    }
+
+    public class BMWBuilder : IBuilder
+    {
+        public override void CreateColor()
+        {
+            car.Color = "red";
+        }
+
+        public override void CreateModel()
+        {
+            car.Model = "2022";
+        }
+
+        public override void CreateName()
+        {
+            car.name = "X6 M Sports Activity";
+        }
+    }
+
+
+    public  class CarAssembler
+    {
+      
+        public Car CarBuild(IBuilder builder)
+        {
+            builder.CreateName();
+            builder.CreateModel();
+            builder.CreateColor();
+            return builder.GetCar();
+        }
+    }
+
+
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using ConsoleApp2.DesignPattern.Builder;
 using ConsoleApp2.Solid.OpenClose;
 
 DiscountProduct discountProduct = new();
@@ -10,3 +11,13 @@ Console.WriteLine(discountProduct2.GetProductPrice(25000));
 
 DiscountProduct discountProduct3 = new(new silverUser());
 Console.WriteLine(discountProduct3.GetProductPrice(25000));
+
+///////////////////////////
+///Builder
+///
+CarAssembler carAssembler = new CarAssembler();
+
+var BmwCar = carAssembler.CarBuild(new BMWBuilder());
+Console.WriteLine($"car {nameof(BmwCar.name)} : {BmwCar.name} || {nameof(BmwCar.Color)} : {BmwCar.Color}");
+
+
