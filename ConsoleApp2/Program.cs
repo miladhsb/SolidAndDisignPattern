8cc2c7edf9806d824378756797cc44abab3c58d8
milad khoshkam <milad@gmail.com>
2022-07-01 13:24:49 +0430

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp2.Solid.OpenClose;
+
+DiscountProduct discountProduct = new();
+Console.WriteLine(discountProduct.GetProductPrice(25000));
+
+DiscountProduct discountProduct2 = new(new GoldentUser());
+Console.WriteLine(discountProduct2.GetProductPrice(25000));
+
+DiscountProduct discountProduct3 = new(new silverUser());
+Console.WriteLine(discountProduct3.GetProductPrice(25000));
---
+++ ConsoleApp2/Solid/OpenClose/DiscountProduct.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Solid.OpenClose
+{
+    public class DiscountProduct
+    {
+        private readonly UserType _userType;
+
+        public DiscountProduct():this(new UserType())
+        {
+          
+        }
+        public DiscountProduct(UserType userType)
+        {
+            this._userType = userType;
+        }
+
+        public double GetProductPrice(double Price)
+        {
+            return Price * (double)_userType.GetUserDiscount();
+        }
+    }
+
+     public  class UserType
+    {
+       public virtual decimal GetUserDiscount() 
+        {
+            return 1;
+        }
+    }
+
+
+
+    public class GoldentUser : UserType
+    {
+        public override decimal GetUserDiscount()
+        {
+            return (decimal)0.02;
+        }
+    }
+    public  class silverUser : UserType
+    {
+        public override decimal GetUserDiscount()
+        {
+            return (decimal)0.9;
+        }
+    }
+
+
+}
