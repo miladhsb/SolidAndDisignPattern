594fa5e6efd2aa3b0bb9b66e0ad8c10fb2b2caa1
milad khoshkam <milad@gmail.com>
2022-07-01 14:07:14 +0430

    FluentBuilder

---
+++ ConsoleApp2/DesignPattern/Builder/FluentBuilder.cs
@@ -0,0 +1,51 @@
+﻿using ConsoleApp2.DesignPattern.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.DesignPattern.FluentBuilder
+{
+
+
+    public abstract class CarBuilder
+    {
+        protected Car car { get; set; }=new Car();
+        public abstract CarBuilder CreateName();
+        public abstract CarBuilder CreateModel();
+        public abstract CarBuilder CreateColor();
+
+        public Car GetCar()
+        {
+            return car;
+        }
+
+    }
+
+    public class BMWBuilder : CarBuilder
+    {
+        public override CarBuilder CreateColor()
+        {
+            car.Color = "red";
+            return this;
+        }
+
+        public override CarBuilder CreateModel()
+        {
+            car.Model = "2022";
+            return this;
+        }
+
+        public override CarBuilder CreateName()
+        {
+            car.name = "X3 Sports Activity";
+            return this;
+        }
+    }
+
+
+  
+
+
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using ConsoleApp2.DesignPattern.Builder;
+using ConsoleApp2.DesignPattern.FluentBuilder;
 using ConsoleApp2.Solid.OpenClose;
 
 DiscountProduct discountProduct = new();
@@ -17,7 +18,15 @@ Console.WriteLine(discountProduct3.GetProductPrice(25000));
 ///
 CarAssembler carAssembler = new CarAssembler();
 
-var BmwCar = carAssembler.CarBuild(new BMWBuilder());
+var BmwCar = carAssembler.CarBuild(new ConsoleApp2.DesignPattern.Builder.BMWBuilder());
 Console.WriteLine($"car {nameof(BmwCar.name)} : {BmwCar.name} || {nameof(BmwCar.Color)} : {BmwCar.Color}");
 
 
+//////////////////////
+///FluentBuilder
+///
+
+CarBuilder BMWBuilder = new ConsoleApp2.DesignPattern.FluentBuilder.BMWBuilder();
+var BmwCar2 = BMWBuilder.CreateName().CreateModel().CreateColor().GetCar();
+
+Console.WriteLine($"car {nameof(BmwCar2.name)} : {BmwCar2.name} || {nameof(BmwCar2.Color)} : {BmwCar2.Color}");
